5e2219b74c9091fe03e09d461487c46c361e4c67
SarojKapali <81839032+saroj-kapali@users.noreply.github.com>
2024-05-12 20:15:19 -0500

    new commit

---
+++ functions.js
@@ -0,0 +1,6 @@
+function addnumber(x,y){
+    const z = x+y;
+    return z;
+}
+
+console.log(addnumber(4, 5));
---
+++ loop.js
@@ -0,0 +1,7 @@
+for(let a = 1;a<=5;a++){
+    let toPrint = "";
+    for(let b =1; b<=a;b++){
+        toPrint +=b+'   ';
+    }
+    console.log(toPrint);
+}
